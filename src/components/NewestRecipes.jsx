9a7f04093e7cfc7f106cfb00b65bb58da63375a7
JakubCukrowski <j.d.cukrowski@gmail.com>
2023-12-03 21:01:35 +0100

    Added basic searh option for searchbar, couple styles added

--- src/components/NewestRecipes.jsx
+++ src/components/NewestRecipes.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import {Container } from "react-bootstrap";
+import {Button, Container } from "react-bootstrap";
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import { StyledCol } from "../styles/CardStyles/StyledCol";
 import { StyledCard } from "../styles/CardStyles/StyledCard";
 import { StyledCardImg } from "../styles/CardStyles/StyledCardImg";
+import { FlexContainer } from "../styles/Containers";
 
 
 export const NewestRecipes = ({recipes}) => {
@@ -17,21 +18,24 @@ export const NewestRecipes = ({recipes}) => {
     return (
         <Container>
             <h2 style={{textAlign: "center", padding: 60}}>Najnowsze przepisy</h2>
-            <Row xs={1} md={2} className="g-4">
+            <Row xs={1} md={2} className="g-4" style={{paddingBottom: 20}}>
                 {sorted.splice(0, 4).map((recipe, idx) => (
                     <StyledCol key={idx}>
-                    <StyledCard>
-                        <StyledCardImg variant="top" src={recipe.image} />
-                        <Card.Body>
-                        <Card.Title>{recipe.name}</Card.Title>
-                        <Card.Text>
-                            {`Polubienia: ${recipe.likes}`}
-                        </Card.Text>
-                        </Card.Body>
-                    </StyledCard>
+                        <StyledCard>
+                            <StyledCardImg variant="top" src={recipe.image} />
+                            <Card.Body>
+                            <Card.Title>{recipe.name}</Card.Title>
+                            <Card.Text>
+                                {`Liczba polubień: ${recipe.likes}`}
+                            </Card.Text>
+                            </Card.Body>
+                        </StyledCard>
                     </StyledCol>
                 ))}
-                </Row>
+            </Row>
+            <FlexContainer justify="center">
+                <Button variant="dark">Zobacz więcej</Button>
+            </FlexContainer>
         </Container>
     )
 }
--- src/styles/CardStyles/StyledCard.js
+++ src/styles/CardStyles/StyledCard.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import { Card } from "react-bootstrap";
 
 export const StyledCard = styled(Card)`
+    position: relative;
+
     @media (min-width: 768px) {
         height: 100%;
     }
