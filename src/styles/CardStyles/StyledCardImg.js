ee456d16a05222f6956d840e958d773cbaf8f063
JakubCukrowski <j.d.cukrowski@gmail.com>
2023-12-05 14:54:58 +0100

    Extended searchbar options

--- src/styles/CardStyles/StyledCardImg.js
+++ src/styles/CardStyles/StyledCardImg.js
@@ -2,5 +2,9 @@ import styled from "styled-components";
 import { CardImg } from "react-bootstrap";
 
 export const StyledCardImg = styled(CardImg)`
-    height: 250px;
+    height: 220px;
+
+    @media (min-width: 992px) {
+        height: 250px;
+    }
 `
--- src/styles/HeroStyles/StyledSearchedRecipes.js
+++ src/styles/HeroStyles/StyledSearchedRecipes.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledSearchedRecipes = styled.ul`
     background-color: white;
     width: calc(100% - 80px);
-    height: 200px;
+    max-height: 180px;
     border: 1px solid;
     text-align: left;
     list-style: none;
@@ -11,5 +11,44 @@ export const StyledSearchedRecipes = styled.ul`
     position: absolute;
     top: 60%;
     left: 0;
-    padding: 20px;
+    padding: 0;
+    overflow-y: auto;
+
+    
+    &::-webkit-scrollbar {
+    width: 10px;
+    }
+
+    &::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px grey; 
+    border-radius: 10px;
+    }
+    
+    &::-webkit-scrollbar-thumb {
+    background: rgb(33, 37, 41); 
+    border-radius: 10px;
+    }
+
+    &::-webkit-scrollbar-thumb:hover {
+    background: #b30000; 
+    }
+    
+
+    li {
+        padding: 10px;
+
+        &:nth-child(1n) {
+            background-color: rgba(190, 190, 190, 0.5);
+        }
+
+        &:nth-child(2n) {
+            background-color: rgba(167, 167, 167, 0.5);
+        }
+
+        a {
+            text-decoration: none;
+            color: black;
+            display: block;
+        }
+    }
 `
