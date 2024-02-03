9833c2386117d322fb7f32f022cca627a6bd84bf
JakubCukrowski <j.d.cukrowski@gmail.com>
2024-02-03 14:20:38 +0100

    Validation on preparation steps added

--- src/components/recipes/Preparation.jsx
+++ src/components/recipes/Preparation.jsx
@@ -1,28 +1,52 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { Button, Form, FormGroup } from "react-bootstrap";
+import { Alert, Button, Form, FormGroup } from "react-bootstrap";
 
-export const Preparation = ({ details, addNextStep, handleStepsObject }) => {
+export const Preparation = ({
+  details,
+  addNextStep,
+  handleStepsObject,
+  errors,
+  handleStepsErrors,
+}) => {
   const handleInputChange = (e, step) => {
     const newStepsObject = details.preparationSteps;
-    // Object.values(newStepsObject)[0] = e.target.value;
-    Object.keys(newStepsObject).forEach(key => {
+    Object.keys(newStepsObject).forEach((key) => {
       if (key === step) {
-        newStepsObject[key] = e.target.value
+        newStepsObject[key] = e.target.value;
       }
-    })
-    handleStepsObject(newStepsObject)
+    });
+    handleStepsObject(newStepsObject);
+
+    //clear errors
+    const newErrorsArray = errors.preparationStepsErrors;
+    newErrorsArray[step] = false;
+    handleStepsErrors(newErrorsArray);
   };
 
   return (
     <>
       <p>Daj znać innym jak przygotować potrawę</p>
-      {Object.entries(details.preparationSteps).map((step) => {
+      {Object.entries(details.preparationSteps).map((step, index) => {
         return (
           <FormGroup key={step[0]} className="mb-3">
             <Form.Label>Krok {parseInt(step[0]) + 1}</Form.Label>
-            <Form.Control as='textarea' style={{resize: "none"}} rows={4} onChange={(e) => handleInputChange(e, step[0])} />
+            {errors.preparationStepsErrors[index] && step[1].length === 0 ? (
+              <Alert variant="danger">Nie może być puste</Alert>
+            ) : null}
+            {errors.preparationStepsErrors[index] && (step[1].length > 0 && step[1].length < 15) ? (
+              <Alert variant="danger">Ten krok jest za krótki</Alert>
+            ) : null}
+            <Form.Control
+              isInvalid={
+                errors.preparationStepsErrors[index] && step[1].length === 0
+              }
+              as="textarea"
+              style={{ resize: "none" }}
+              rows={4}
+              onChange={(e) => handleInputChange(e, step[0])}
+            />
           </FormGroup>
         );
       })}
