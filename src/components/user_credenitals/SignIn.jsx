1c212953a6836cb8569c45c5fc648ecf02c7aaad
JakubCukrowski <j.d.cukrowski@gmail.com>
2024-02-04 16:56:17 +0100

    Added check if account exists

--- src/components/user_credenitals/SignIn.jsx
+++ src/components/user_credenitals/SignIn.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
-import { FlexContainer } from '../../styles/Containers';
+import { Form, Button, Alert } from "react-bootstrap";
+import { FlexContainer } from "../../styles/Containers";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { StyledLink } from "../../styles/StyledLink";
@@ -10,65 +10,130 @@ import { useNavigate } from "react-router-dom";
 import { StyledSignSection } from "./StyledSignSection";
 
 export const SignIn = () => {
-    const {login} = UserAuth()
-    const navigate = useNavigate()
-    const [userData, setUserData] = useState({
-        email: '',
-        password: ''
-    })
+  const { login } = UserAuth();
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+  const [credentialsErrors, setCredentialsErrors] = useState({
+    email: false,
+    password: false,
+  });
 
-    const handleInputs = (e) => {
-        const {name, value} = e.target
+  const handleInputs = (e) => {
+    const { name, value } = e.target;
 
-        setUserData(prev => {
+    setUserData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+
+    setCredentialsErrors((prev) => {
+      return {
+        ...prev,
+        [name]: false,
+      };
+    });
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    setCredentialsErrors((prev) => {
+      return {
+        ...prev,
+        email: userData.email.length === 0 ? true : prev.email,
+        password: userData.password.length === 0 ? true : prev.password,
+      };
+    });
+
+    if (!credentialsErrors.email && !credentialsErrors.password) {
+      try {
+        await login(userData.email, userData.password);
+        navigate("/dashboard");
+      } catch (error) {
+        if (error.message === "Firebase: Error (auth/invalid-email).") {
+          setCredentialsErrors((prev) => {
             return {
-                ...prev,
-                [name]: value
-            }
-        })
-    }
-
-    const handleLogin = async (e) => {
-        e.preventDefault()
-
-        try {
-            await login(userData.email, userData.password)
-            navigate('/dashboard')
-        } catch (error) {
-            console.log(error);
+              email: true,
+              password: true,
+            };
+          });
         }
+      }
     }
+  };
 
-    return (
-        <StyledSignSection>
-            <StyledLink color="white" style={{padding: 20, alignSelf: "flex-start"}} to='/'><FontAwesomeIcon icon={faArrowLeft}/> Strona główna</StyledLink>
-            <FlexContainer direction='column' align='center'>
-                <h2 style={{color: "white", padding: 26}}>Zaloguj się</h2>
-                <span style={{margin: 10}}>Nie masz konta? <StyledLink to='/signup'>Zarejestruj się</StyledLink></span>
-                    <StyledForm>
-                        <Form.Group className="mb-3" controlId="formGroupEmail">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control
-                                onChange={handleInputs} 
-                                value={userData.email} 
-                                name="email" 
-                                type="email" 
-                                placeholder="Twój email" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formGroupPassword">
-                            <Form.Label>Hasło</Form.Label>
-                            <Form.Control 
-                                onChange={handleInputs} 
-                                value={userData.password} 
-                                name="password" 
-                                type="password" 
-                                placeholder="Hasło" />
-                        </Form.Group>
-                        <Button onClick={handleLogin} variant="light" type="submit">
-                            Potwierdź
-                        </Button>
-                    </StyledForm>  
-                </FlexContainer>    
-        </StyledSignSection>
-    )
-}
+  return (
+    <StyledSignSection>
+      <StyledLink
+        color="white"
+        style={{ padding: 20, alignSelf: "flex-start" }}
+        to="/"
+      >
+        <FontAwesomeIcon icon={faArrowLeft} /> Strona główna
+      </StyledLink>
+      <FlexContainer direction="column" align="center">
+        <h2 style={{ color: "white", padding: 26 }}>Zaloguj się</h2>
+        <span style={{ margin: 10 }}>
+          Nie masz konta? <StyledLink to="/signup">Zarejestruj się</StyledLink>
+        </span>
+        <StyledForm>
+          {credentialsErrors.email &&
+          credentialsErrors.password &&
+          userData.email.length > 0 &&
+          userData.password.length > 0 ? (
+            <Alert variant="danger">Nie znaleźliśmy konta</Alert>
+          ) : null}
+          <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Label>Email</Form.Label>
+            {credentialsErrors.email && userData.email.length === 0 ? (
+              <Alert variant="danger">Musisz podać email</Alert>
+            ) : null}
+            <Form.Control
+              isInvalid={
+                (credentialsErrors.email && userData.email.length === 0) ||
+                (credentialsErrors.email &&
+                  credentialsErrors.password &&
+                  userData.email.length > 0 &&
+                  userData.password.length)
+              }
+              onChange={handleInputs}
+              value={userData.email}
+              name="email"
+              type="email"
+              placeholder="Twój email"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formGroupPassword">
+            <Form.Label>Hasło</Form.Label>
+            {credentialsErrors.password && userData.password.length === 0 ? (
+              <Alert variant="danger">Musisz podać hasło</Alert>
+            ) : null}
+            <Form.Control
+              isInvalid={
+                (credentialsErrors.password &&
+                  userData.password.length === 0) ||
+                (credentialsErrors.email &&
+                  credentialsErrors.password &&
+                  userData.email.length > 0 &&
+                  userData.password.length)
+              }
+              onChange={handleInputs}
+              value={userData.password}
+              name="password"
+              type="password"
+              placeholder="Hasło"
+            />
+          </Form.Group>
+          <Button onClick={handleLogin} variant="light" type="submit">
+            Potwierdź
+          </Button>
+        </StyledForm>
+      </FlexContainer>
+    </StyledSignSection>
+  );
+};
