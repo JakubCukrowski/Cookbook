cf90087309153aa0de50e1f886645dfd7e0d8637
JakubCukrowski <j.d.cukrowski@gmail.com>
2024-02-12 19:41:58 +0100

    Added prep time to filtering

--- src/components/recipes/form_elements/FormCategory.jsx
+++ src/components/recipes/form_elements/FormCategory.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Form } from "react-bootstrap";
 
-export const FormCategory = ({isInvalid, value, name, id, onChange}) => {
+export const FormCategory = ({isInvalid, value, name, id, onChange, optionDisabled}) => {
   return (
     <>
       <Form.Select
@@ -11,7 +11,7 @@ export const FormCategory = ({isInvalid, value, name, id, onChange}) => {
         id={id}
         onChange={onChange}
       >
-        <option value="default">wybierz</option>
+        <option value="default" disabled={optionDisabled}>wybierz</option>
         <option value="Dania główne">Dania główne</option>
         <option value="Zupy">Zupy</option>
         <option value="Śniadania">Śniadania</option>
