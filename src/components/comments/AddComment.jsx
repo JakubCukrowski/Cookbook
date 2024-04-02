d6156458ae50c65a85902b3a407c9e89039e24b6
JakubCukrowski <j.d.cukrowski@gmail.com>
2024-04-02 21:43:31 +0200

    Comments are being added. Part 1 finished. Delete, edit, like, unlike, block to like if users comment need to be added yet.

--- src/components/comments/AddComment.jsx
+++ src/components/comments/AddComment.jsx
@@ -1,28 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { FlexContainer } from "../../styles/Containers";
 import Form from "react-bootstrap/Form";
 import { UserAuth } from "../../context/AuthContext";
 import { Link } from "react-router-dom";
 import { AddCommentButton, StyledInputGroup } from "./commentsStyles";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { db } from "../../firebase";
+import { Alert } from "react-bootstrap";
 
-export const AddComment = () => {
+export const AddComment = ({ searchedRecipe }) => {
   const { user } = UserAuth();
+  const recipeRef = doc(db, "recipes", searchedRecipe.id);
+  const [comment, setComment] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleInput = (e) => {
+    setComment(e.target.value);
+    setError(false);
+  };
+
+  const handleAddRecipe = async (e) => {
+    e.preventDefault();
+
+    if (comment.length < 5) {
+      setError(true);
+    } else {
+      await updateDoc(recipeRef, {
+        comments: arrayUnion({
+          user: user.displayName,
+          userPhoto: user.photoURL,
+          comment: comment,
+          addDate: new Date().toLocaleDateString(),
+          likes: 0,
+        }),
+      });
+      setComment("");
+    }
+  };
 
   return (
     <FlexContainer justify="center" align="center" direction="column">
-      <h3 style={{ marginBottom: 30 }}>Przepis nie ma jeszcze komentarzy</h3>
+      <h3 style={{ marginBottom: 30 }}>
+        {searchedRecipe.comments.length > 0
+          ? "Komentarze"
+          : "Przepis nie ma jeszcze komentarzy"}
+      </h3>
 
       {user ? (
         <>
           <p>Dodaj komentarz</p>
+          {error && comment.length < 5 ? (
+            <Alert variant="danger">Za krótko</Alert>
+          ) : null}
           <StyledInputGroup>
             <Form.Control
+              isInvalid={error && comment.length < 5}
+              onChange={handleInput}
+              value={comment}
               style={{ resize: "none", width: "100%", height: 120 }}
               size="sm"
               as="textarea"
               aria-label="With textarea"
             />
-            <AddCommentButton onClick={() => alert("Pracujemy nad tym")}>
+            <AddCommentButton onClick={handleAddRecipe}>
               Opublikuj
             </AddCommentButton>
           </StyledInputGroup>
