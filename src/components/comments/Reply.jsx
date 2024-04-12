b443f75319dc3d349bc27dbd0a97553f1fe12918
JakubCukrowski <j.d.cukrowski@gmail.com>
2024-04-12 17:37:24 +0200

    Added comments, and replies rating

--- src/components/comments/Reply.jsx
+++ src/components/comments/Reply.jsx
@@ -1,67 +1,60 @@
-import React from "react";
-import {
-  CommentWrapper,
-  Content,
-  UserImage,
-  UserName,
-  AddDate,
-  LikesWrapper,
-  CommentLikes,
-  CommentButtonsWrapper,
-  DeleteCommentButton,
-  EditCommentButton,
-  CommentDataWrapper,
-  ReplyContainer,
-  ReplyWrapper,
-} from "./commentsStyles";
-import { OrangeButton } from "../../styles/OrangeButton";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPlus,
-  faMinus,
-  faReply,
-  faTrashCan,
-  faEdit,
-} from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import { ReplyContainer, ReplyWrapper } from "./commentsStyles";
+import { CommentStructure } from "./CommentStructure";
 import { UserAuth } from "../../context/AuthContext";
+import { updateDoc } from "firebase/firestore";
 
-export const Reply = ({ data, index, calculateElapsedTimeInMinutes }) => {
+export const Reply = ({
+  data,
+  index,
+  calculateElapsedTimeInMinutes,
+  recipeComments,
+  recipeRef,
+}) => {
   const { user } = UserAuth();
+
+  const handleLikeReply = async () => {
+    //customize comments object and then update it in firebase
+    const comments = recipeComments;
+    const reply = comments[index].replies.find(
+      (reply) => reply.comment === data.comment
+    );
+    const ratedBy = reply.ratedBy;
+
+    if (user) {
+      if (!ratedBy.includes(user.displayName)) {
+        ratedBy.push(user.displayName);
+
+        await updateDoc(recipeRef, {
+          comments: comments,
+        });
+      } else {
+        const dislike = ratedBy.filter(
+          (username) => username !== user.displayName
+        );
+  
+        reply.ratedBy = dislike;
+      }
+
+      await updateDoc(recipeRef, {
+        comments: comments,
+      });
+    }
+  };
 
   return (
     <ReplyContainer>
       <ReplyWrapper>
-        <CommentDataWrapper>
-          <UserImage src={data.userPhoto} alt="user" />
-          <UserName>{data.user}</UserName>
-          <AddDate>{calculateElapsedTimeInMinutes(data.addDate)}</AddDate>
-          <Content>{data.comment}</Content>
-        </CommentDataWrapper>
-        <LikesWrapper>
-          <OrangeButton onClick={() => handleLikeComment(index)}>
-            <FontAwesomeIcon icon={faPlus} />
-          </OrangeButton>
-          <CommentLikes>{data.ratedBy.length}</CommentLikes>
-          <OrangeButton onClick={() => handleDislikeComment(index)}>
-            <FontAwesomeIcon icon={faMinus} />
-          </OrangeButton>
-        </LikesWrapper>
-        <CommentButtonsWrapper>
-          {user && user.displayName === data.user ? (
-            <>
-              <DeleteCommentButton>
-                <FontAwesomeIcon icon={faTrashCan} /> {""} Skasuj
-              </DeleteCommentButton>
-              <EditCommentButton>
-                <FontAwesomeIcon icon={faEdit} /> {""} Edytuj
-              </EditCommentButton>
-            </>
-          ) : (
-            <EditCommentButton>
-              <FontAwesomeIcon icon={faReply} /> <br /> {""} Odpowiedz
-            </EditCommentButton>
-          )}
-        </CommentButtonsWrapper>
+        <CommentStructure
+          userImage={data.userPhoto}
+          userName={data.user}
+          elapsedTime={calculateElapsedTimeInMinutes(data.addDate)}
+          commentContent={data.comment}
+          handleLikeComment={() => handleLikeReply()}
+          handleDislikeComment={() => handleLikeReply()}
+          commentLikes={data.ratedBy.length}
+          disabled={data.ratedBy.includes(user.displayName)}
+        />
       </ReplyWrapper>
     </ReplyContainer>
   );
