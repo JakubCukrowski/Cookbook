39a8b34b73c593e00cd314d8bc7a0e2dcce1c122
JakubCukrowski <j.d.cukrowski@gmail.com>
2024-07-15 21:23:07 +0200

    Added formik validation on sign in and up, added function to check if username is taken

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,110 +1,140 @@
 import React, { useEffect, useState } from "react";
 import { UserAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
-import { useFormik } from "formik";
-import { AuthForm } from "../components/AuthForm";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { Container } from "@mui/system";
+import {
+  StyledForm,
+  StyledSignSection,
+  StyledTextField,
+} from "../assets/styles/CredentialsStyles";
+import bckImg from "../assets/images/signForm2.jpg";
+import { Box, FormControl, FormHelperText, Typography } from "@mui/material";
+import { StyledLink } from "../assets/styles/StyledLink";
+import { ConfirmButton } from "../assets/styles/Buttons";
+import CircularProgressPage from "./CircularProgressPage";
 
 export const SignIn = () => {
-  const { login, user } = UserAuth();
+  const { login } = UserAuth();
   const navigate = useNavigate();
   const [loggingIn, setLoggingIn] = useState(false);
-  const [saveEmail, setSaveEmail] = useState(false)
-
-  const validate = (values) => {
-    const errors = {};
-    if (!values.email) {
-      errors.email = "Musisz podać email";
-    } else if (
-      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-    ) {
-      errors.email = "Niepoprawny adres email";
-    }
-
-    if (!values.password) {
-      errors.password = "Musisz podać hasło";
-    }
-
-    return errors;
-  };
-
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: ""
-    },
-    validate,
-    onSubmit: async (values, { setErrors }) => {
-      try {
-        setLoggingIn(true);
-        await login(values.email, values.password);
-        navigate(-1);
-        if (saveEmail) localStorage.setItem('email', formik.values.email)
-      } catch (error) {
-        setLoggingIn(false);
-        if (error.code === "auth/invalid-credential") {
-          setErrors({ loginStatus: "Niepoprawny email lub hasło" });
-        }
-        if (error.code === "auth/too-many-requests") {
-          setErrors({ loginStatus: "Spróbuj ponownie później" });
-        }
-      }
-    }
-  });
-
-  const inputs = [
-    {
-      inputErrors: (formik.touched.email && formik.errors.email),
-      errorContent: formik.errors.email,
-      id: "email",
-      label: "Email",
-      value: formik.values.email,
-      name: "email",
-      type: "email",
-      inputFillError: (formik.touched.email && formik.errors.email) || formik.errors.loginStatus,
-      onChange: formik.handleChange,
-      onBlur: formik.handleBlur,
-    },
-    {
-      inputErrors: formik.touched.password && formik.errors.password,
-      errorContent: formik.errors.password,
-      id: "password",
-      label: "Hasło",
-      value: formik.values.password,
-      name: "password",
-      type: "password",
-      inputFillError: (formik.touched.password && formik.errors.password) || formik.errors.loginStatus,
-      onChange: formik.handleChange,
-      onBlur: formik.handleBlur,
-    },
-  ];
-
-  const saveEmailInLocalStorage = () => {
-    setSaveEmail(prev => !prev)
-  }
-
-  useEffect(() => {
-    console.log(localStorage.getItem('email') !== null);
-    if (localStorage.getItem('email') !== null) {
-      formik.values.email = localStorage.getItem('email')
-      setSaveEmail(true)
-    }
-  }, [user])
 
   return (
     <>
-      <AuthForm
-        title="Zaloguj się"
-        accountExists="Nie masz konta?"
-        linkTo="Utwórz konto"
-        href="/signup"
-        handleSubmit={formik.handleSubmit}
-        signInComponent={true}
-        inputs={inputs}
-        loginStatus={formik.errors.loginStatus}
-        loggingIn={loggingIn}
-        rememberEmail={saveEmailInLocalStorage}
-        saveEmail={saveEmail}
-      />
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+          accountExists: "",
+        }}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .email("Niepoprawny adres email")
+            .required("Musisz podać email"),
+          password: Yup.string().required("Musisz podać hasło"),
+        })}
+        onSubmit={async (values, { setFieldError }) => {
+          try {
+            setLoggingIn(true);
+            await login(values.email, values.password);
+            navigate(-1);
+            // if (saveEmail) localStorage.setItem("email", formik.values.email);
+          } catch (error) {
+            setLoggingIn(false);
+            if (error.code === "auth/invalid-credential") {
+              setFieldError("accountExists", "Niepoprawny email lub hasło");
+            }
+            if (error.code === "auth/too-many-requests") {
+              setFieldError(
+                "accountExists",
+                "Zbyt wiele nieudanych prób. Spróbuj ponownie później."
+              );
+            }
+          }
+        }}
+      >
+        {(formik) => (
+          <StyledSignSection backgroundimage={bckImg}>
+            {loggingIn ? (
+              <CircularProgressPage />
+            ) : (
+              <Container sx={{ zIndex: 200 }}>
+                <StyledForm onSubmit={formik.handleSubmit} noValidate>
+                  <Typography variant="h4">Zaloguj się</Typography>
+                  <Typography>
+                    Nie masz konta?{" "}
+                    <StyledLink to={"/signup"}>Zarejestruj się</StyledLink>
+                  </Typography>
+                  {formik.errors.accountExists ? (
+                    <FormHelperText error>
+                      {formik.errors.accountExists}
+                    </FormHelperText>
+                  ) : null}
+                  <FormControl
+                    fullWidth
+                    error={formik.errors.email && formik.touched.email}
+                  >
+                    <StyledTextField
+                      label="Email"
+                      id="email"
+                      name="email"
+                      type="email"
+                      error={
+                        (formik.errors.email && formik.touched.email) ||
+                        formik.errors.accountExists
+                      }
+                      helperText={
+                        formik.errors.email && formik.touched.email
+                          ? formik.errors.email
+                          : null
+                      }
+                      {...formik.getFieldProps("email")}
+                    />
+                  </FormControl>
+                  <FormControl
+                    fullWidth
+                    error={formik.errors.password && formik.touched.password}
+                  >
+                    <StyledTextField
+                      label="Hasło"
+                      id="password"
+                      name="password"
+                      type="password"
+                      error={
+                        (formik.errors.password && formik.touched.password) ||
+                        formik.errors.accountExists
+                      }
+                      helperText={
+                        formik.errors.password && formik.touched.password
+                          ? formik.errors.password
+                          : null
+                      }
+                      {...formik.getFieldProps("password")}
+                    />
+                  </FormControl>
+                  <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: "center"}}>
+                    <ConfirmButton variant="contained" type="submit">
+                      Zaloguj
+                    </ConfirmButton>
+                    <ConfirmButton
+                      variant="contained"
+                      type="button"
+                      onClick={async () => {
+                        setLoggingIn(true);
+                        await login("test@test.com", "123456");
+                        navigate(-1);
+                      }}
+                    >
+                      konto demo
+                    </ConfirmButton>
+                  </Box>
+                </StyledForm>
+              </Container>
+            )}
+          </StyledSignSection>
+        )}
+      </Formik>
     </>
   );
 };
